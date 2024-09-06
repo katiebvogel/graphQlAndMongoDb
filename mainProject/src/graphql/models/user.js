e7c429fc6cf1551ad5d8204589842c0fcda22c24
Katie Vogel <kvogel@2ndswing.com>
2024-09-06 15:52:39 -0500

    added mutations for create, delete, udpate

--- mainProject/src/graphql/models/user.js
+++ mainProject/src/graphql/models/user.js
@@ -1,47 +1,91 @@
+import {ObjectId} from 'mongodb';
+
 export const userTypeDef = /* GraphQL */`
     type Query {
-        user: User
+        users: [User!]!
+
+        user(id: ID!): User
     }
 
     type Mutation {
         createUser(user: NewUserInput!): User
+        deleteUser(id: ID!): Boolean
+        updateUser(id: ID!, update: UpdateUserInput): User
     }
 
     input NewUserInput {
         name: String!
-        age: Int!
+        email: String!
+    }
+
+    input UpdateUserInput {
+        name: String!
     }
 
     type User {
-        id: Int
+        id: ID!
         name: String,
-        age: Int
+        email: String
     }
 `;
 
 export const userResolvers = {
     Query: {
-        user: () => {
-            return {
-                id: 1,
-                name: '    Katie  ',
-                age: 8
-            }
+
+        users: (obj, args, { mongo}) => {
+
+            return mongo.users.find().limit(5).toArray();
+        },
+
+        user: (obj, {id}, { mongo }) => {
+            console.log('id', id);
+            const user = mongo.users.findOne({_id: new ObjectId(id)});
+            return user;
         }
     },
 
+
     Mutation: {
-        createUser: async(_, {user}, {mongo }) => {
-            const movies = await mongo.movies.find().toArray();
-            console.log('movies', movies);
+        createUser: async(_, {user}, { mongo }) => {
+
+            const response = await mongo.users.insertOne(user);
+            console.log('response', response);
             return {
-                id:1,
-                ...user
+                id: 1,
+                ...user,
             };
         },
+
+        deleteUser: async(obj, {id}, {mongo}) => {
+           const response =  await mongo.users.deleteOne({_id: new ObjectId(id)});
+           console.log(response);
+            return true;
+        },
+
+        updateUser: async(obj, {id, update}, {mongo}) => {
+            const response = await mongo.users.updateOne(
+                {_id: new ObjectId(id)}, 
+                {$set: {name: update.name}}
+            );
+            console.log(response);
+            return mongo.users.findOne({_id: new ObjectId(id)});
+        }
+
+        //ex mutation in Ruru
+        /** mutation UpdateUser {
+         updateUser(id: "59b99dc8cfa9a34dcd7885de", update: {name: "hello"}) {
+            id
+        name
+        email
+        }
+        } */
+
     },
 
     User: {
+        id: ({id, _id}) => {
+            return _id || id;
+        },
         name: (obj) => {
             return obj.name.trim().toUpperCase();
         }
